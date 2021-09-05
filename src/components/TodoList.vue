1f1eb80344c94af05d4681f9a93ea54e55936de7
Fayzulla Rahmatullayev <80602071+fayzullarakhmatullayev@users.noreply.github.com>
2021-09-05 17:00:27 +0500

    update

---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <ul>
+            
+            <TodoItems 
+                v-for="(todo, i) of todos"
+                :todo="todo"
+                :key="todo.id"
+                v-bind:index="i" 
+                v-on:remove-todo="removeTodo"
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoItems from "@/components/TodoItem";
+export default{
+    props: ['todos'],
+    components: {
+        TodoItems
+    },
+    methods: {
+        removeTodo(id) {
+            this.$emit('remove-todo', id)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    ul{
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
