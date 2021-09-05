1388de8a4b7156a7c747f35adf77099277dd45ae
Fayzulla Rahmatullayev <80602071+fayzullarakhmatullayev@users.noreply.github.com>
2021-09-05 16:48:39 +0500

    update

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>Home Page</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi unde, blanditiis dolorum vel voluptatem atque consequuntur eveniet, tempore veritatis totam aliquam architecto in ipsum sequi porro distinctio iste. Quaerat, laboriosam!</p>
+        
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
